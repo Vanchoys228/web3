9537de0dc0bd95749462da8c8505437ce5cc00ec
Vanek <murawiew2005@gmail.com>
2024-05-25 09:11:29 +0700

    123

--- main.py
+++ main.py
@@ -20,9 +20,7 @@ if not os.path.exists(static_dir):
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 def split_image_and_generate_histograms(image_bytes):
-    # Загрузить изображение из байтов
     image = Image.open(io.BytesIO(image_bytes))
-    # Разделить изображение на 4 части
     width, height = image.size
     left = 0
     top = 0
@@ -32,12 +30,10 @@ def split_image_and_generate_histograms(image_bytes):
     img2 = image.crop((right, top, width, bottom))
     img3 = image.crop((left, bottom, right, height))
     img4 = image.crop((right, bottom, width, height))
-    # Сохранить новые изображения
     img1.save(f"{static_dir}/part1.jpg")
     img2.save(f"{static_dir}/part2.jpg")
     img3.save(f"{static_dir}/part3.jpg")
     img4.save(f"{static_dir}/part4.jpg")
-    # Создать гистограммы для каждого изображения
     fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 8))
     axes = [ax1, ax2, ax3, ax4]
     for i, img in enumerate([img1, img2, img3, img4]):
@@ -81,6 +77,4 @@ async def split_and_plot(request: Request, file: UploadFile = File(...), resp: s
         return templates.TemplateResponse("index.html", {"request": request, "detail": [{"type": "error", "msg": str(e)}], "captcha_passed": False, "title": "FastAPI Веб-сайт - Ошибка"})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
-
+    uvicorn.run(app, host="localhost", port=8000)
